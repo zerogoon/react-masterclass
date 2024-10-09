d9a063ac697b566fe0e652298146e8933f6cccdf
zerogoon <kckzero@gmail.com>
2024-10-09 18:27:50 +0900

    update 1009

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,5 @@
-import styled from "styled-components";
-
-const Title = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
-const Wrapper = styled.div`
-  display: flex;
-  height: 100vh;
-  width: 100vw;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props) => props.theme.backgroundColor};
-`;
-
 function App() {
-  return (
-    <Wrapper>
-      <Title>Hello</Title>
-    </Wrapper>
-  );
+  return null;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
+import { darkTheme, lightTheme } from "./theme";
 
-const darkTheme = {
-  textColor: "whitesmoke",
-  backgroundColor: "#111",
-};
-
-const lightTheme = {
-  textColor: "#111",
-  backgroundColor: "whitesmoke",
-};
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
   <React.StrictMode>
     <ThemeProvider theme={darkTheme}>
       <App />
     </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
